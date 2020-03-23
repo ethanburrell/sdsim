3d8a16e8736ad6018674e692507e03379fdd45b6
Ethan Burrell <30448729+ethanburrell@users.noreply.github.com>
2020-03-23 00:49:54 -0700

    initial commit

---
+++ Assets/AddTerrainInFront.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTerrainInFront : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Terrain terrain;
+    void Start()
+    {
+        GameObject t = GameObject.FindGameObjectsWithTag("terrain1")[0];
+        Debug.Log("AddTerrainInFront " + this.transform.position);
+        t.transform.position = new Vector3(this.transform.position.x - .5f * this.transform.position.x, 0, this.transform.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/move_random.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_random : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float x;
+    public float y;
+    public float z;
+    Vector3 pos;
+    public GameObject obj;
+    void Start()
+    {
+        x = Random.Range(-25, 26);
+        y = 5;
+        z = Random.Range(-25, 26);
+        pos = new Vector3(x, y, z);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Random.Range(-25, 26);
+        y = 5;
+        z = Random.Range(-25, 26);
+        pos = new Vector3(x, y, z);
+        transform.position = pos;
+    }
+}
